ab6a25e252f443a1d8a2b12b50ad92a4d7e8eed5
aNobre-redhat <anobre@redhat.com>
2024-10-25 21:37:02 -0300

    changing flask to run with guinicorn

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["python", "app.py"]
+# Use Gunicorn para rodar a aplicação em produção
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -45,3 +45,7 @@ def delete_file(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+@app.before_request
+def log_request():
+    print(f"Received request: {request.method} {request.path}")
