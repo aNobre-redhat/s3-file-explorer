364fa65fb7d8bfb42bbeb4cc183c106b5bf1f982
aNobre-redhat <anobre@redhat.com>
2024-10-26 07:39:36 -0300

    first test for file viewer.

--- app.py
+++ app.py
@@ -37,7 +37,13 @@ def upload_file():
 @app.route('/list', methods=['GET'])
 def list_files():
     response = s3_client.list_objects_v2(Bucket=bucket_name)
-    files = [obj['Key'] for obj in response.get('Contents', [])]
+    files = [
+        {
+            'name': obj['Key'],
+            'url': s3_client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': obj['Key']}, ExpiresIn=3600)
+        }
+        for obj in response.get('Contents', [])
+    ]
     return jsonify(files), 200
 
 @app.route('/delete/<filename>', methods=['DELETE'])
@@ -51,3 +57,8 @@ if __name__ == '__main__':
 @app.before_request
 def log_request():
     print(f"Received request: {request.method} {request.path}")
+
+@app.route('/view/<filename>', methods=['GET'])
+def view_file(filename):
+    file_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': bucket_name, 'Key': filename}, ExpiresIn=3600)
+    return jsonify({"url": file_url}), 200
